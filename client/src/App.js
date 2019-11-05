8cb7b27d9497f81f9bdb3767c613951f66ed9ecb
romluc <romulofilhofilho@gmail.com>
2019-11-05 15:19:22 -0300

    Create ShoppingList component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Hello!</h1>
+			<AppNavbar />
+			<ShoppingList />
 		</div>
 	);
 }
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -22,21 +22,23 @@ class AppNavbar extends Component {
 	};
 
 	render() {
-		<div>
-			<Navbar color='dark' dark expand='sm' className='mb-5'>
-				<Container>
-					<NavbarBrand href='/'>ShoppingList</NavbarBrand>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className='ml-auto' navbar>
-							<NavItem>
-								<NavLink href='https://github.com/romluc'>GitHub</NavLink>
-							</NavItem>
-						</Nav>
-					</Collapse>
-				</Container>
-			</Navbar>
-		</div>;
+		return (
+			<div>
+				<Navbar color='dark' dark expand='sm' className='mb-5'>
+					<Container>
+						<NavbarBrand href='/'>ShoppingList</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className='ml-auto' navbar>
+								<NavItem>
+									<NavLink href='https://github.com/romluc'>GitHub</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Container>
+				</Navbar>
+			</div>
+		);
 	}
 }
 
