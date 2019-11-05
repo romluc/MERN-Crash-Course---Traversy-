320e31738ad316c7c26f4789a45e60159019e5a7
romluc <romulofilhofilho@gmail.com>
2019-11-05 14:22:35 -0300

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://rom:rom123@cluster0-f4h8g.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo fixing deprecation warnings
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose
+	.connect(db)
+	.then(() => console.log('MongoDB connected...'))
+	.catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
