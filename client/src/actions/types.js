f274c4cde0ecc23625449f9c216473bbf4464830
romluc <romulofilhofilho@gmail.com>
2019-11-14 11:09:30 -0300

    Create cases in authReducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,7 +3,6 @@ export const ADD_ITEM = "ADD_ITEM";
 export const DELETE_ITEM = "DELETE_ITEM";
 export const ITEMS_LOADING = "ITEMS_LOADING";
 export const USER_LOADING = "USER_LOADING";
-export const USER_LOADING = "USER_LOADING";
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
